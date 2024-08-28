47cc43f824ee5527ade0671560837b1946f8fc52
macdonald34 <144123866+macdonald34@users.noreply.github.com>
2024-08-29 01:55:29 +0530

    Update HomePage.js

--- car/src/components/HomePage.js
+++ car/src/components/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div>
       <h1>Welcome to Car Rentals</h1>
-      <p>Find your perfect car for rent.</p>
+      <p>Find your perfect car for rent any time any day.</p>
       <h2>Luxury Car Hire</h2>
     </div>
   );
